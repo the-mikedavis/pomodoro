fac66cd07bf4aefdd62932657415d3e340280042
Michael Davis <rubiksmaster65@yahoo.com>
2017-12-03 02:30:34 -0500

    Get pomodoro to send appropriate responses

--- src/pomodoro/core.clj
+++ src/pomodoro/core.clj
@@ -3,14 +3,19 @@
   (:require
     [clj-slack-client
      [core :as slack]
+     [rtm-transmit :as tx]
      [team-state :as state]
      [web :as web]]
     [pomodoro.file-ops :as store]
     [clj-time.core :as time]
+    [clojure.string :as string]
     [org.httpkit.client :as http]))
+
 
 (def api-token-filename "api-token.txt")
 (def ^:dynamic *api-token* nil)
+
+;(defn get-lunch-channel-id [] (:id (state/name->channel lunch-channel-name)))
 
 (defn get-user-dm-id
   "get the direct message channel id for this user.
@@ -19,6 +24,99 @@
   (if-let [dm-id (state/user-id->dm-id user-id)]
     dm-id
     (web/im-open *api-token* user-id)))
+
+(defn contextualize-command
+  "apply slack message context to the raw command"
+  [cmd {requestor :user, text :text, ts :ts, channel-id :channel, :as msg} cmd-text]
+  (-> cmd
+      (assoc :requestor requestor)
+      (assoc :text text)
+      (assoc :cmd-text cmd-text)
+      (assoc :channel-id channel-id)
+      (assoc :ts ts)))
+
+(defn printex
+  [msg ex]
+  (let [line (apply str (repeat 100 "-"))]
+    (println line)
+    (println msg)
+    (println ex)
+    (clojure.stacktrace/print-stack-trace ex)
+    (println line)))
+
+(defn get-command-signature-re []
+  "returns a regex that will match a command signature, indicating
+  that the user wants lunchbot to interpret the message as a command"
+  (let [linkified-self (tx/linkify (state/self-id))]
+    (re-pattern (str linkified-self ":?"))))
+
+
+(defn message->command-text
+  "determines if the message should be interpreted as a command, and if so, returns
+  the command text from the message."
+  [channel-id text]
+  (when text
+    (let [cmd-signature-re (get-command-signature-re)
+          has-cmd-signature (re-find cmd-signature-re text)]
+      (when (or (state/dm? channel-id) has-cmd-signature)
+        (-> text
+            (string/replace cmd-signature-re "")
+            (string/trim))))))
+
+(defn handle-command
+  "translates the command into events, commits the events to the stream,
+  handles the events, and returns replies."
+  [cmd]
+  ;(println cmd)
+  cmd)
+
+(defn parse-command
+  "Set the command type based on the input text"
+  [input]
+  (hash-map :command-type (if (re-find #"zen" input)
+                            (if (or (re-find #"start" input)
+                                    (re-find #"end" input))
+                              (if (re-find #"start" input)
+                                :start
+                                :end))
+                            :unrecognized)))
+
+(defn formulate-response
+  "Give a string response to a command map and start async functions"
+  [command]
+  (case (:command-type command)
+    :start "Focus for 25 minutes."
+    :end "I've ended your timer."
+    :unrecognized "I didn't catch that."))
+
+(defn handle-message
+  "translates a slack message into a command, handles that command, and communicates the reply"
+  [{channel-id :channel, text :text, :as msg}]
+  (try
+    (when-let [cmd-text (message->command-text channel-id text)]
+      (let [raw-cmd (parse-command cmd-text); {:command-type :unrecognized}
+            cmd (contextualize-command raw-cmd msg cmd-text)
+            reply (formulate-response cmd)]
+        (tx/say-message channel-id reply)))
+    (catch Exception ex
+      (printex (str "Exception trying to handle slack message\n" (str msg) ".") ex)
+      (try (tx/say-message channel-id "@!#?@!")))))
+
+(comment
+  (defn handle-message
+    "translates a slack message into a command, handles that command, and communicates the reply"
+    [{channel-id :channel, text :text, :as msg}]
+    (try
+      (when-let [cmd-text (command/message->command-text channel-id text)]
+        (let [raw-cmd (command/command-text->command cmd-text)
+              cmd (contextualize-command raw-cmd msg cmd-text)
+              replies (handle-command cmd)]
+          (doseq [reply replies]
+            (distribute-message reply))))
+      (catch Exception ex
+        (printex (str "Exception trying to handle slack message\n" (str msg) ".") ex)
+        (try (talk/say-message channel-id "@!#?@!")))))
+)
 
 
 (defn dispatch-handle-slack-event [event] ((juxt :type :subtype) event))
@@ -37,6 +135,13 @@
 (defmethod handle-slack-event :default
   [event]
   nil)
+
+(defn try-handle-slack-event
+  [event]
+  (try
+    (handle-slack-event event)
+    (catch Exception ex
+      (printex (str "Exception trying to handle slack event\n" (str event) ".") ex))))
 
 (def heartbeating (atom false))
 
@@ -85,8 +190,6 @@
    (start (store/read-api-token api-token-filename)))
   ([api-token]
    (try
-     (event/initialize-events)
-     (restaurant/initialize-restaurants)
      (alter-var-root (var *api-token*) (constantly api-token))
      (slack/connect *api-token* try-handle-slack-event)
      (start-heartbeat)
