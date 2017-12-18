cd566d26d289db57300089b0d2208a7eab2adeb0
Michael Davis <rubiksmaster65@yahoo.com>
2017-12-18 14:01:45 -0500

    Update deps
    This is possible because of new active developement of the
    clj-slack-client library

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject pomodoro "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Slack bot that helps you focus"
+  :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-slack-client "0.1.6-SNAPSHOT"]
-                 [clj-time "0.9.0"]
-                 [cheshire "5.4.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [clj-slack-client "0.1.7-SNAPSHOT"]
+                 [clj-time "0.14.2"]
+                 [cheshire "5.8.0"]
                  [http-kit "2.2.0"]]
   :main ^:skip-aot pomodoro.core
   :target-path "target/%s"
