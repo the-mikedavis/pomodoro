8e916f6e5d6ee41e0d48d1b0a100ebdb6b898436
Michael Davis <rubiksmaster65@yahoo.com>
2017-12-30 00:04:52 -0500

    Rely on the new dnd module methods of the client
    I've written a dnd module for the clj-slack-api fork. This uses those
    methods to simplify the status dnd file greatly.

--- src/pomodoro/status.clj
+++ src/pomodoro/status.clj
@@ -1,58 +1,44 @@
 ; get the status of Do Not Disturb
 (ns pomodoro.status
-  (:require [pomodoro.interact :as interact]
+  (:require [pomodoro
+             [interact :as interact]
+             [state :as state]]
+            [clj-slack-client.dnd :as dnd]
             [clojure.string :as string]))
-
-(defn parse-int [s]
-  "Convert a string to integer"
-  (Integer. (re-find #"\d+" s)))
-
-(defn time-difference
-  "Gets the time in minutes between two time stamps. Assumes that start
-  is a string and end is an integer."
-  [end start]
-  (quot (- end (parse-int start)) 60))
-
-(defn get-remaining-time
-  "Return the number of minutes left on a Do Not Disturb. Compares the
-  time stamps of the message recieved and the response of the
-  dnd.info call"
-  [msg response]
-  (time-difference (:next_dnd_end_ts response)
-            (:ts msg)))
 
 (defn tell-remaining-time
   "Generate a string with the time remaining on a Do Not Disturb"
   [msg response]
   (if (:dnd_enabled response)
-    (str "The pomodoro is running. It has "
-         (get-remaining-time msg response)
-         " minutes remaining.")
+    (let [t (dnd/get-remaining-time (:ts msg) response)]
+      (str "The pomodoro is running. It has " (:min t)
+           " minutes and " (:sec t) " seconds remaining."))
     "No pomodoro is running."))
-
-(defn get-dnd-response
-  "Get the time left on Do Not Disturb for the user in the message"
-  ([msg user] ; single person
-   (interact/call-and-get-response "dnd.info"
-                                   {:user user}))
-  ([msg]      ; the team
-   (interact/call-and-get-response "dnd.teamInfo")))
 
 (defn get-user-dnd-status
   "Get the reply string of the time left on Do Not Disturb for the
   user in the message. The second arity is for if you already have
   the json response"
   [msg]
-   (tell-remaining-time msg (get-dnd-response msg (:user msg))))
-
+  (tell-remaining-time
+    msg
+    (dnd/get-dnd-response state/api-token
+                          (:user msg))))
 
 (defn get-team-dnd-status
   "Gets the reply string for the whole team's dnd status."
   [msg]
-  (let [resp (get-dnd-response msg)]
-    (->> (:users resp)
-         (map (fn [[user-id api-response]]
-                (str (interact/get-name user-id)
-                     ": "
-                     (tell-remaining-time msg api-response))))
-         (string/join "\n"))))
+  (->> (dnd/get-dnd-response state/api-token)
+       :users
+       (map (fn [[user-id api-response]]
+              (str (interact/get-name user-id)
+                   ": "
+                   (tell-remaining-time msg api-response))))
+       (string/join "\n")))
+;  (let [resp (dnd/get-dnd-response state/api-token)]
+;    (->> (:users resp)
+;         (map (fn [[user-id api-response]]
+;                (str (interact/get-name user-id)
+;                     ": "
+;                     (tell-remaining-time msg api-response))))
+;         (string/join "\n"))))
